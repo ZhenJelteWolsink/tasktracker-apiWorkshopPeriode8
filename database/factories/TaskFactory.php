8139abf72741af63c76c34fd9483f020333626c6
Zhen-Jelte Wolsink <zhenjelte.wolsink@student.graafschapcollege.nl>
2026-05-08 14:43:44 +0200

    Eerste commit

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+use App\Models\Task;
+
+class TaskFactory extends Factory
+
+{
+
+    protected $model = Task::class;
+
+    public function definition(): array
+
+    {
+
+        return [
+
+            'title' => fake()->sentence(3),
+
+            'is_done' => false,
+
+        ];
+
+    }
+
+}
